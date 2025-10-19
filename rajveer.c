b8e58f9c85edabfdae50376aa22447d2532678c9
rajveersingh-07 <220728482+rajveersingh-07@users.noreply.github.com>
2025-10-19 20:45:59 +0530

    Initial commit of my C programs

--- rajveer.c
+++ rajveer.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 int main() 
-{ float base, height, area;
-    printf("Enter the base and height of triangle\n");
-    scanf("%f%f", &base, &height);
-    area = 0.5 * base * height;
-    printf("The area of the triangle is %f\n", area);
-    float r;
-    printf("Enter the radius of circle\n");
-    scanf("%f", &r);
-    float a = 3.14 * r * r; 
-
-    printf("The area of the circle is %f\n", a);    
+{ int a=10, b=30, c;
+  printf("before swapping %d %d\n",a,b);
+  c=a;a=b;b=c;
+  printf("after swapping %d %d\n",a,b);
   return 0;
 }
