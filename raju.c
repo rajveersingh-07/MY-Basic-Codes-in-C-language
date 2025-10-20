24db72892604f74eb6693faeffc8774a936f9b4a
rajveersingh-07 <220728482+rajveersingh-07@users.noreply.github.com>
2025-10-20 11:09:14 +0530

    Added simple interest calculator

--- raju.c
+++ raju.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main() 
-{ 
-    float principle, interest, rate ,time;
+int main()
+{
+    float principle, interest, rate, time;
     printf("Enter Principle amount: \n enter rate of interest: \n enter time in years: \n");
-    scanf("%f %f %f",&principle , &rate , &time);
-    interest= (principle*rate*time)/100;
-    printf("The Simple Interest is: %.2f\n",interest);
-    return 0;   
+    scanf("%f %f %f", &principle, &rate, &time);
+    interest = (principle * rate * time) / 100;
+    printf("The Simple Interest is: %.2f\n", interest);
+    return 0;
 }
